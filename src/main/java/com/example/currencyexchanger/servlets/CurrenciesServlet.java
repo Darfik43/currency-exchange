8fa524164ce54143eeb43691f4163f7ade611321
darfik <darfik43@gmail.com>
2023-10-20 00:18:28 +0300

    Add servlets

---
+++ src/main/java/com/example/currencyexchanger/servlets/CurrenciesServlet.java
@@ -0,0 +1,10 @@
+package com.example.currencyexchanger.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "index currencies servlet", value = "/currencies")
+public class CurrenciesServlet extends HttpServlet {
+
+
+}
---
+++ src/main/java/com/example/currencyexchanger/servlets/CurrencyServlet.java
@@ -0,0 +1,4 @@
+package com.example.currencyexchanger.servlets;
+
+public class CurrencyServlet {
+}
